5c4671fbb24cee5a31d76ef637726ecd73fbf525
Ninja AZ <lovepinklol123@gmail.com>
2020-06-17 16:40:19 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import Testimonials from './pages/Testimonials';
+import Contact from './pages/Contact';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 import {Route, Switch} from 'react-router-dom';
@@ -17,6 +18,7 @@ function App() {
         <Route exact path="/" component={Home}/>
         <Route exact path="/rooms" component={Rooms}/>
         <Route exact path="/testimonials" component={Testimonials}/>
+        <Route exact path="/contact" component={Contact}/>
         <Route exact path="/rooms/:slug" component={SingleRoom}/>
         <Route component={Error}/>
       </Switch>
